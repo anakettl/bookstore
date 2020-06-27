c8ee4b4a4364352318d4ff118b2843f3b377af20
anakettl <anakettl@gmail.com>
2020-06-27 18:48:36 -0300

    Create delete product route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  delete "products/:id", to: "products#destroy", as: :product
   get "products/new", to: "products#new"
   post "products/new", to: "products#create"
   root to: "products#index"
