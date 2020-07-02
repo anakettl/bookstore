97d0380c903c6436df23bdf0d5fab1a497182a02
anakettl <anakettl@gmail.com>
2020-07-01 21:18:31 -0300

    Add department table and foreing key department_id in product table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_27_153643) do
+ActiveRecord::Schema.define(version: 2020_07_01_232713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "departments", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -23,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_06_27_153643) do
     t.integer "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "department_id"
   end
 
 end
