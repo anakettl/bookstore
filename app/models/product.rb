fe0fd4e8aaaada77333e8e2e49e72818fc467fde
anakettl <anakettl@gmail.com>
2020-07-01 21:24:36 -0300

    Add belongs_to department

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
-  validates_presence_of :name, :author, :description, :price, :amount, message: "Can't be blank"
+  belongs_to :department
+
+  validates_presence_of :name, :author, :description, :price, :amount, :department, message: "Can't be blank"
   validates :amount, numericality: { only_integer: true }
 end
