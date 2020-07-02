8997a785c30faf6fffb54c4e533300b250e6a6b4
anakettl <anakettl@gmail.com>
2020-07-01 21:15:34 -0300

    Add department resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :departments
   get "products/search", to: "products#search", as: :search_product
   resources :products, only: [:new, :create, :destroy]
   root to: "products#index"
