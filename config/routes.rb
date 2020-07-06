14a609fee2b026ea64c023d0d3e8bef5fdea6789
anakettl <anakettl@gmail.com>
2020-07-05 22:05:12 -0300

    Create route to update product

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :departments
   get "products/search", to: "products#search", as: :search_product
-  resources :products, only: [:new, :create, :destroy]
+  resources :products, only: [:new, :create, :destroy, :edit, :update]
   root to: "products#index"
 end
