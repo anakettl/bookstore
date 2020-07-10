6e2f78fe1e80c1d38f1d57e01355ac83723d5e67
anakettl <anakettl@gmail.com>
2020-07-09 21:36:12 -0300

    Refactoring routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :departments
   get "products/search", to: "products#search", as: :search_product
-  resources :products, only: [:new, :create, :destroy, :edit, :update]
+  resources :products
   root to: "products#index"
 end
