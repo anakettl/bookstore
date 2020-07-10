261277fa3e7d147e606b28155e92da9b9b9fd0ba
anakettl <anakettl@gmail.com>
2020-07-09 22:42:53 -0300

    Refactoring tests with department

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,37 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Product, :type => :model do
-  subject { 
-    described_class.new(
-      name: 'O mito de Sísifo',
-      author: 'Albert Camus',
-      description: 'De um dos mais importantes e representativos autores do século XX e Prêmio Nobel de Literatura.Albert Camus, um dos escritores e intelectuais mais influentes do século XX, publicou O mito de Sísifo em 1942. Este ensaio sobre o absurdo tornou-se uma importante contribuição filosófico-existencial e exerceu profunda influência sobre toda uma geração.',
-      price: 32.44,
-      amount: 5) 
-  } 
+  before(:each) do
+    @department = FactoryBot.create(:department)
+    @product = FactoryBot.create(:product, department: @department)
+	end
 
   it 'is valid with valid attributes' do
-    expect(subject).to be_valid
+    expect(@product).to be_valid
   end
   it 'is not valid without name' do
-    subject.name = nil
-    expect(subject).to_not be_valid
+    @product.name = nil
+    expect(@product).to_not be_valid
   end
   it 'is not valid without author' do
-    subject.author = nil
-    expect(subject).to_not be_valid
+    @product.author = nil
+    expect(@product).to_not be_valid
   end
   it 'is not valid without description' do
-    subject.description = nil
-    expect(subject).to_not be_valid
+    @product.description = nil
+    expect(@product).to_not be_valid
   end
   it 'is not valid without price' do
-    subject.price = nil
-    expect(subject).to_not be_valid
+    @product.price = nil
+    expect(@product).to_not be_valid
   end
   it 'is not valid without amount' do
-    subject.amount = nil
-    expect(subject).to_not be_valid 
+    @product.amount = nil
+    expect(@product).to_not be_valid 
   end
 
 end
