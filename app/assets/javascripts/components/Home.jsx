335f9ae785298b360319b97d35a1dec0fab7fe40
anakettl <anakettl@gmail.com>
2020-08-08 11:17:42 -0300

    wip

---
+++ app/assets/javascripts/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default () => (
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="container secondary-color">
+        <h1 className="display-4">Food Recipes</h1>
+        <p className="lead">
+          Test js component
+        </p>
+        <hr className="my-4" />
+      </div>
+    </div>
+  </div>
+);
